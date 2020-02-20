b11fe7b705bf0cf6c74e920e9d9d7b94f24c467c
David Jungblut <davidobob@laposte.net>
2020-02-20 18:57:23 +0100

    cocktails solved without style

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# puts "creating ingredients"
+
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
+# Ingredient.create(name: "orange juice")
+# Ingredient.create(name: "tonic")
+
+# puts "Done !"
+
+puts "creating cocktails"
+Cocktail.create(name: "Mojito")
+puts "done!"
